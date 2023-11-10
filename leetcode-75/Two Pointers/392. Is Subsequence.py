b7b0c103000e625d96cf188ef3a69840e2e87299
jq <jq@gmail.com>
2023-11-10 23:53:41 +0800

    add 392

---
+++ leetcode-75/Two Pointers/392. Is Subsequence.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        i1 = 0
+        i2 = 0
+        while i1 < len(s) and i2 < len(t):
+            if s[i1] == t[i2]:
+                i1 += 1
+            i2 += 1
+        return i1 == len(s)
+        
